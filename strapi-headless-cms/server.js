8821a409b317969e53299b381ff057d4df206120
Chua Shaomin <e0191435@u.nus.edu>
2020-07-22 14:56:14 +0800

    change in server.js & package.json

--- strapi-headless-cms/server.js
+++ strapi-headless-cms/server.js
@@ -1,3 +1,7 @@
 const strapi = require('strapi');
-
+const port = process.env.PORT || 3000;
+const app = strapi();
+app.listen(port, () => {
+  console.log(`Web server up on port ${port}`);
+})
 strapi(/* {...} */).start();
