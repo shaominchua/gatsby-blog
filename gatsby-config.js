188feefe0e306e572e1dcea52816fbeff5b4b26d
Chua Shaomin <e0191435@u.nus.edu>
2020-07-27 14:02:54 +0800

    changing apiURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,8 +41,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: process.env.DEPLOY_URL ? 'https://strapi-headless-cms.herokuapp.com' :'http://localhost:1337',
-        // apiURL:"https://strapi-headless-cms.herokuapp.com",
+        // apiURL: process.env.DEPLOY_URL ? 'https://strapi-headless-cms.herokuapp.com' :'http://localhost:1337',
+        apiURL:"http://localhost:1337",
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'articles',
           'users',
